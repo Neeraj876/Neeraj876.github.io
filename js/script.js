ae7d304ef7f84d2e07b472ffc69ad126d18370a8
Neeraj876 <94380338+Neeraj876@users.noreply.github.com>
2025-03-28 21:03:38 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 /* ============================== typing animation ============================ */
 var typed = new Typed(".typing",{
-    strings:["","MLOps Engineer","Data Scientist","GenAI Engineer","ML Engineer"],
+    strings:["","MLOps Engineer", "ML Engineer"],
     typeSpeed:100,
     BackSpeed:60,
     loop:true
